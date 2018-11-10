471e9a556719b83b556d93bb624f7304db06baf1
Krzysztof Syska <prezydium834@gmail.com>
2018-11-10 15:21:37 +0100

    Added basic functionality - play toast1

--- app/src/main/java/org/prezydium/nakasprowy/MainActivity.java
+++ app/src/main/java/org/prezydium/nakasprowy/MainActivity.java
@@ -1,13 +1,23 @@
 package org.prezydium.nakasprowy;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.toast1);
+    }
+
+    public void playToast(View view) {
+        view.getId();
+        mediaPlayer.start();
     }
 }
